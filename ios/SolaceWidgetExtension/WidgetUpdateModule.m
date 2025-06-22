1c1394c31bffe65b8248a5ea477a458692ff9e1b
Dorghaam <dorghaam.416@gmail.com>
2025-06-22 19:59:44 -0400

    added widget

--- ios/SolaceWidgetExtension/WidgetUpdateModule.m
+++
@@ -1,8 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(WidgetUpdateModule, NSObject)
-
-RCT_EXTERN_METHOD(updateQuotes:(NSArray<NSString *> *)quotes)
-RCT_EXTERN_METHOD(updateUserName:(NSString *)userName)
-
-@end 
