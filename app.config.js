2c3f3061b485123852e9e282916b1bf0bb857e78
Dorghaam <dorghaam.416@gmail.com>
2025-06-23 19:59:23 -0400

    updated app icon

--- app.config.js
+++ app.config.js
@@ -11,11 +11,11 @@ export default ({ config }) => ({
     version: "1.0.0",
     scheme: "solaceapp",
     orientation: "portrait",
-    icon: "./assets/app-icon.png", // Updated to new app icon
+    icon: "./icon.png", // Updated to new app icon
     userInterfaceStyle: "light",
     newArchEnabled: true,
     splash: {
-      image: "./assets/app-icon.png", // Updated to use new app icon
+      image: "./icon.png", // Updated to use new app icon
       resizeMode: "contain",
       backgroundColor: "#FFF5F7" // Updated to pink gradient lightest color
     },
@@ -36,7 +36,7 @@ export default ({ config }) => ({
     android: {
       ...config.expo?.android,
       adaptiveIcon: {
-        foregroundImage: "./assets/app-icon.png",
+        foregroundImage: "./icon.png",
         backgroundColor: "#FFF5F7" // Updated to match pink theme
       },
       package: "com.dorghaamhaidar.solace"
