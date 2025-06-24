3372f831b891594f97ac96da544c3c86054b1aa7
Dorghaam <dorghaam.416@gmail.com>
2025-06-24 10:33:24 -0400

    Updated categories to be smooth on first render.

--- components/CategoriesModal.tsx
+++ components/CategoriesModal.tsx
@@ -19,6 +19,51 @@ import { BreakupCategory, breakupInterestCategories, useUserStore } from '@/stor
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
+// Preload images at module level
+const CATEGORY_IMAGES = {
+  'general_healing': require('../categoryImages/6425304.jpg'),
+  'moving_on': require('../categoryImages/image copy.png'), 
+  'moving_forward': require('../categoryImages/image.png'),
+  'self_love': require('../categoryImages/4038942.jpg'),
+  'self_love_discovery': require('../categoryImages/Happy smiling woman admiring beautiful reflection in mirror.jpg'),
+  'coping_loneliness': require('../categoryImages/image copy 3.png'),
+  'overcoming_loneliness': require('../categoryImages/7853915.jpg'),
+  'rebuilding_confidence': require('../categoryImages/image copy 4.png'),
+  'managing_anger_resentment': require('../categoryImages/image copy 5.png'),
+  'finding_closure': require('../categoryImages/image copy 6.png'),
+  'finding_peace': require('../categoryImages/39093.jpg'),
+  'hope_for_future': require('../categoryImages/image copy 7.png'),
+  'hope_future': require('../categoryImages/4365672.jpg'),
+  'healing_from_betrayal': require('../categoryImages/image copy 8.png'),
+  'loss_of_partner_widow': require('../categoryImages/4590471.jpg'),
+  'navigating_divorce': require('../categoryImages/3349694.jpg'),
+  'heartbreak_recovery': require('../categoryImages/te07_qq4a_141122.jpg'),
+  'letting_go_of_ex': require('../categoryImages/8107017.jpg'),
+  'letting_go': require('../categoryImages/4582464.jpg'),
+  'letting_go_acceptance': require('../categoryImages/8593071.jpg'),
+  'embracing_single_life': require('../categoryImages/4098100.jpg'),
+  'overcoming_codependency': require('../categoryImages/image copy 2.png'),
+  'gratitude_reflection': require('../categoryImages/8620602.jpg')
+};
+
+const CATEGORY_COLORS = {
+  'general_healing': theme.colors.categoryColors.pink,
+  'moving_on': theme.colors.categoryColors.blue,
+  'self_love_discovery': theme.colors.categoryColors.lavender,
+  'coping_loneliness': theme.colors.categoryColors.coral,
+  'rebuilding_confidence': theme.colors.categoryColors.orange,
+  'managing_anger_resentment': theme.colors.categoryColors.teal,
+  'finding_closure': theme.colors.categoryColors.purple,
+  'hope_for_future': theme.colors.categoryColors.green,
+  'healing_from_betrayal': theme.colors.categoryColors.coral,
+  'loss_of_partner_widow': theme.colors.categoryColors.blue,
+  'navigating_divorce': theme.colors.categoryColors.orange,
+  'heartbreak_recovery': theme.colors.categoryColors.pink,
+  'letting_go_of_ex': theme.colors.categoryColors.lavender,
+  'embracing_single_life': theme.colors.categoryColors.green,
+  'overcoming_codependency': theme.colors.categoryColors.teal
+};
+
 interface Category {
   id: string;
   title: string;
@@ -30,55 +75,11 @@ interface Category {
 
 // Map breakup categories to UI categories with icons and colors
 const mapBreakupCategoriesToUI = (breakupCategories: BreakupCategory[], subscriptionTier: string, activeQuoteCategory: string | null): Category[] => {
-  const iconMap: { [key: string]: any } = {
-    'general_healing': require('../categoryImages/6425304.jpg'),
-    'moving_on': require('../categoryImages/image copy.png'), 
-    'moving_forward': require('../categoryImages/image.png'),
-    'self_love': require('../categoryImages/4038942.jpg'),
-    'self_love_discovery': require('../categoryImages/Happy smiling woman admiring beautiful reflection in mirror.jpg'),
-    'coping_loneliness': require('../categoryImages/image copy 3.png'),
-    'overcoming_loneliness': require('../categoryImages/7853915.jpg'),
-    'rebuilding_confidence': require('../categoryImages/image copy 4.png'),
-    'managing_anger_resentment': require('../categoryImages/image copy 5.png'),
-    'finding_closure': require('../categoryImages/image copy 6.png'),
-    'finding_peace': require('../categoryImages/39093.jpg'),
-    'hope_for_future': require('../categoryImages/image copy 7.png'),
-    'hope_future': require('../categoryImages/4365672.jpg'),
-    'healing_from_betrayal': require('../categoryImages/image copy 8.png'),
-    'loss_of_partner_widow': require('../categoryImages/4590471.jpg'),
-    'navigating_divorce': require('../categoryImages/3349694.jpg'),
-    'heartbreak_recovery': require('../categoryImages/te07_qq4a_141122.jpg'),
-    'letting_go_of_ex': require('../categoryImages/8107017.jpg'),
-    'letting_go': require('../categoryImages/4582464.jpg'),
-    'letting_go_acceptance': require('../categoryImages/8593071.jpg'),
-    'embracing_single_life': require('../categoryImages/4098100.jpg'),
-    'overcoming_codependency': require('../categoryImages/image copy 2.png'),
-    'gratitude_reflection': require('../categoryImages/8620602.jpg')
-  };
-
-  const colorMap: { [key: string]: string } = {
-    'general_healing': theme.colors.categoryColors.pink,
-    'moving_on': theme.colors.categoryColors.blue,
-    'self_love_discovery': theme.colors.categoryColors.lavender,
-    'coping_loneliness': theme.colors.categoryColors.coral,
-    'rebuilding_confidence': theme.colors.categoryColors.orange,
-    'managing_anger_resentment': theme.colors.categoryColors.teal,
-    'finding_closure': theme.colors.categoryColors.purple,
-    'hope_for_future': theme.colors.categoryColors.green,
-    'healing_from_betrayal': theme.colors.categoryColors.coral,
-    'loss_of_partner_widow': theme.colors.categoryColors.blue,
-    'navigating_divorce': theme.colors.categoryColors.orange,
-    'heartbreak_recovery': theme.colors.categoryColors.pink,
-    'letting_go_of_ex': theme.colors.categoryColors.lavender,
-    'embracing_single_life': theme.colors.categoryColors.green,
-    'overcoming_codependency': theme.colors.categoryColors.teal
-  };
-
   return breakupCategories.map(category => ({
     id: category.id,
     title: category.label,
-    icon: iconMap[category.id] || '💭',
-    color: colorMap[category.id] || theme.colors.categoryColors.pink,
+    icon: CATEGORY_IMAGES[category.id] || '💭',
+    color: CATEGORY_COLORS[category.id] || theme.colors.categoryColors.pink,
     locked: category.premium && subscriptionTier !== 'premium',
     isSelected: activeQuoteCategory === category.id
   }));
@@ -243,12 +244,8 @@ export const CategoriesModal: React.FC<CategoriesModalProps> = ({
 
   const categories = mapBreakupCategoriesToUI(breakupInterestCategories, subscriptionTier, activeQuoteCategory);
 
-  if (!visible) {
-    return null;
-  }
-
   return (
-    <View style={styles.overlay}>
+    <View style={[styles.overlay, !visible && { pointerEvents: 'none' }]}>
       {/* Background overlay */}
       <Animated.View 
         style={[
