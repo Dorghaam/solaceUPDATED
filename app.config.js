c83c400edad77b75544d5cee467fc87a54bb3211
Dorghaam <dorghaam.416@gmail.com>
2025-06-28 18:11:21 -0400

    updated build number and version to 2.0

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default ({ config }) => ({
     ...config.expo,
     name: "Solace",
     slug: "solace",
-    version: "1.0.11",
+    version: "2.0.0",
     scheme: "solaceapp",
     orientation: "default",
     icon: "./icon.png", // Updated to new app icon
@@ -25,7 +25,7 @@ export default ({ config }) => ({
         isTabletOnly: false,
         requireFullScreen: true,
         bundleIdentifier: "com.dorghaamhaidar.solace.iphone",
-        buildNumber: "14",
+        buildNumber: "15",
         googleServicesFile: "./ios/GoogleService-Info.plist", // Correct path
         storeKitConfigurationPath: "./Product_StoreKit_Config.storekit",
         entitlements: {
