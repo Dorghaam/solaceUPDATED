1321d4e4a6d9038b459be5bcb6f23f77ef97d52e
Dorghaam <dorghaam.416@gmail.com>
2025-06-30 13:20:18 -0400

    upped build number

--- app.config.js
+++ app.config.js
@@ -25,7 +25,7 @@ export default ({ config }) => ({
         isTabletOnly: false,
         requireFullScreen: true,
         bundleIdentifier: "com.dorghaamhaidar.solace.iphone",
-        buildNumber: "16",
+        buildNumber: "17",
         googleServicesFile: "./ios/GoogleService-Info.plist", // Correct path
         storeKitConfigurationPath: "./Product_StoreKit_Config.storekit",
         entitlements: {
